9868e71775a0b99a0b973912e155d7ed12184e66
SimonBelzile <simon.belzile@gmail.com>
2025-11-04 19:29:42 -0500

    Initial commit - clean Astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import optimizer from "astro-google-fonts-optimizer";
 
 export default defineConfig({
   site: "https://cfplateau-website-production.up.railway.app",
@@ -8,12 +7,5 @@ export default defineConfig({
   output: 'static',
   integrations: [
     tailwind(),
-    optimizer(),            // OK après tailwind
-    // … d’autres intégrations si besoin
   ],
-  vite: {
-      ssr: {
-        noExternal: ['astro-google-fonts-optimizer'],  // ← important
-      },
-    },
 });
